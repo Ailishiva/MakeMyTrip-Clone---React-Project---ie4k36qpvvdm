916b9d969e267a039a2236b170aee43bf96bc41c
Aili shivaprasad <aili.shivaprasad@gmail.com>
2023-01-23 10:52:06 +0530

    last commited

--- src/components/Signuppage.jsx
+++ src/components/Signuppage.jsx
@@ -60,9 +60,9 @@ const Signuppage = () =>{
                                           alert("password length should be greater than five");
                             }else{
                                           localStorage.setItem("userData",JSON.stringify([...data,inpval]));// inpval k andr hmne name email wgerah store kiya h
-                                          localStorage.setItem("username", ["name"]);
                                           alert('User successfully registered');
                                           historys("/Signin");
+                                        //  localStorage.setItem("username", ["name"]);
                             }
 
 
