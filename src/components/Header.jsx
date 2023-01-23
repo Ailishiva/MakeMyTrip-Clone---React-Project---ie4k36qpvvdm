cc880f3afcbbe2032d29b367dd4fa7795d4b4d9a
Aili shivaprasad <aili.shivaprasad@gmail.com>
2023-01-23 15:49:46 +0530

    last commited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 import React,{useState}  from 'react';
 import mmtLogoWhite from "../assects/mmtLogoWhite.png"
+import icon from "../assects/icon.jpg"
+import hotelicon from "../assects/hotelicon.png"
+import trainicon from "../assects/trainicon.png"
+
 import '../styles/Header.css';
 import { HashLink } from 'react-router-hash-link';
 // import { Link } from 'react-router-dom';
@@ -16,13 +20,22 @@ function Header() {
         <nav>
        <img className="logo" src={mmtLogoWhite} alt="logo"/> 
       <main >
-        <HashLink to = {"/"}><h6 className='header'>Flights</h6></HashLink>
-        <HashLink to = {"/Hotels"}><h6 className='header'>Hotels</h6></HashLink>
-        <HashLink to = {"/Trains"}><h6 className='header'>Trains</h6></HashLink>
+        <div>
+        <img className='icon' src={icon} alt="icon" style={{height: "30px"}}/>
+        <HashLink to = {"/"}><h5 className='header'>Flights</h5></HashLink>
+        </div>
+        <div>
+        <img className='icon' src={hotelicon} alt="icon" style={{height: "30px"}}/>
+        <HashLink to = {"/Hotels"}><h5 className='header'>Hotels</h5></HashLink>
+        </div>
+        <div className='full'>
+        <img className='icon' src={trainicon} alt="icon" style={{height: "30px"}}/>
+        <HashLink to = {"/Trains"}><h5 className='header'>Trains</h5></HashLink>
+        </div>
         {/* {!hideText ? <div id="about-div">
           <h1>{Arr}</h1>
         </div>:
-        <HashLink to = {"/Signup"}><button className='loginbtn' onClick={onClick}>Log in</button></HashLink>} */}
+       <HashLink to = {"/Signup"}><button className='loginbtn' onClick={onClick}>Log in</button></HashLink>} */}
       </main>
     </nav>
 
